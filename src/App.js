7b3c93cfd3037bf0cf034db4f0680cc0d9a8c544
Joshua Hebert <jhebertcocktails@gmail.com>
2023-10-11 19:15:07 -0400

    initial commit with header changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/aHamptonLogo.svg" className="App-logo" alt="logo" style={ { height: "100px" } } />
+        <h1>Andrew Hampton</h1>
+      </header>
+      <main className="App-main">
+        </main>
+    </div>
+  );
+}
+
+export default App;
